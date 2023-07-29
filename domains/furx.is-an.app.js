db54c1ae820fcbbb6aadcfa2e0fc9159287c1d18
Cality <furrycality@gmail.com>
2023-07-29 15:29:42 -0500

    FurX Domain

---
+++ domains/furx.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+    description: 'Personal proyect',
+    domain: 'is-an.app',
+    subdomain: 'furx',
+    owner: {
+        email: '2.furrycality@gmail.com',
+    },
+    record: {
+        CNAME: '40a3131a-f142-4d47-84de-a1aa262bf9da.id.repl.co',
+        TXT: 'replit-verify=40a3131a-f142-4d47-84de-a1aa262bf9da'
+    },
+    proxy: false,
+})
